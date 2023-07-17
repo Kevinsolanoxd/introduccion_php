06d4d98a83fca827f90a7f42de3c441e7dadfe7b
lizethcifuentes <licethcifuentes@colegioguanenta.edu.co>
2023-07-17 17:45:34 -0500

    comentarios

---
+++ 1_comentarios.php
@@ -0,0 +1,16 @@
+</head>
+<body>
+    <h1>comentarios en php</h1>
+    <!---formas de incluir codigo php--->
+    <?php
+        //comentario de una linea/*esto es un comentaro de varias lineas*/
+    ?>
+     <!---
+    <%
+        codigo php
+    %>
+    <script lenguaje="php">
+
+    </script>--->
+</body>
+</html>
